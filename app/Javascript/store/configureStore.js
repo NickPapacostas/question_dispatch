ac79e2201091b9eabec2d1b0ea6e57d0337f4661
github username <github email address>
2020-04-10 11:29:23 -0400

    deleting Js capital

--- app/Javascript/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "../reducers/index";
-
-const configureStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
-};
-
-export default configureStore;
